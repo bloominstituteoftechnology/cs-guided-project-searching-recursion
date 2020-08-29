54cfb8b3b5378e912a192524c5ca8274a474e9be
jakeduerden <55290573+jakeduerden@users.noreply.github.com>
2020-08-29 16:48:38 -0600

    Update demonstration_3.py

--- src/demonstration_3.py
+++ src/demonstration_3.py
@@ -8,9 +8,9 @@ the jar.
 For example, for a jar of cookies with n = 3 (the jar has 3 cookies inside it),
 there are 4 possible ways for Cookie Monster to eat all the cookies inside it:
 
-He can eat 1 cookie at a time 3 times
-He can eat 1 cookie, then 2 cookies
-He can eat 2 cookies, then 1 cookie
+He can eat 1 cookie at a time, 3 times.
+He can eat 1 cookie, then 2 cookies.
+He can eat 2 cookies, then 1 cookie.
 He can eat 3 cookies all at once.
 Thus, eating_cookies(3) should return an answer of 4.
 
@@ -20,7 +20,7 @@ complexity?
 *Note: Since this question is asking you to generate a bunch of possible
 permutations, you'll probably want to use recursion for this. Think about base
 cases that we would want our recursive function to stop recursing on (when do
-you know you've found a "way" to eat the cookies versus when you have not?).*
+you know you've found a "way" to eat the cookies and when do you not know?).*
 """
 def eating_cookies(n, cache = None):
     # Your code here
